c8baa926fb7a84d0a512644741f76bc87a44fba9
Erik Goranson <esgoranson@gmail.com>
2024-03-29 16:16:08 -0600

    adding official artwork

---
+++ src/components/Artwork.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import InformationSection from './InformationSection.vue';
+import type { PokemonData, } from '@/types';
+
+const props = defineProps({
+  data: {
+    type: Object as () => PokemonData,
+    required: true
+  },
+})
+
+</script>
+
+<template>
+    <InformationSection>
+        <div class="flex items-center justify-center">
+            <img class="" :src="props.data.sprites.other['official-artwork'].front_default"/>
+        </div>
+    </InformationSection>
+</template>
